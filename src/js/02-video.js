f2023def9fd8e91d68a2b9e91cae62a4319d6cfc
sergiihsv <hsv-serg@ukr.net>
2021-12-27 00:29:41 +0200

    2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player.getCurrentTime().then(function (sec) {
+      localStorage.setItem('current-time', JSON.stringify(sec));
+    });
+  }, 1000),
+);
+
+const toStart = JSON.parse(localStorage.getItem('current-time')) || 0;
+player.setCurrentTime(toStart);
